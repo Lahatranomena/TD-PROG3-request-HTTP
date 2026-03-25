87a21f4f1a65e713d9c3e2c2475ab77847339a2e
Lahatranomena <hei.lahatra.3@gmail.com>
2026-03-25 20:56:45 +0300

    feat: add StudentValidator class

--- TD2-TD3-Spring-boot/src/main/java/com/example/demo/validator/StudentValidator.java
+++ TD2-TD3-Spring-boot/src/main/java/com/example/demo/validator/StudentValidator.java
@@ -1,4 +1,24 @@
 package com.example.demo.validator;
 
+import com.example.demo.entity.Student;
+import com.example.demo.exception.BadRequestException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
 public class StudentValidator {
+    public void validate(List<Student> students) {
+        for (Student student : students) {
+            if (student.getReference() == null || student.getReference().isBlank()) {
+                throw new BadRequestException("The reference cannot be null or empty");
+            }
+            if (student.getFirstName() == null || student.getFirstName().isBlank()) {
+                throw new BadRequestException("The firstname cannot be null or empty");
+            }
+            if (student.getLastName() == null || student.getLastName().isBlank()) {
+                throw new BadRequestException("The lastname cannot be null or empty");
+            }
+        }
+    }
 }
